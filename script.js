d739b0faf3a1242718b13e87b2fa9775ba9989cb
josh-jacobsen <joshjacobsen5@gmail.com>
2016-04-13 16:17:28 +1200

    MVP - Player 1 working

---
+++ script.js
@@ -0,0 +1,93 @@
+
+document.addEventListener("keyup", keyUsed);
+
+function keyUsed(event) { 
+  if (event.which === 76){
+    console.log("Key L was pressed");
+    findActive(cells1);
+  } 
+  else if (event.which === 68){
+    console.log("Key D was pressed");
+  }
+}
+
+var player1 = document.getElementById("player1_strip");
+
+var player2 = document.getElementById("player2_strip");
+
+var cells1 = player1.getElementsByTagName("td");
+
+
+function findActive(cells){
+  for (i = 0; i < cells.length; i++){
+    if (cells[i].className === "active"){
+      cells[i].className = "test";
+      cells[i].nextElementSibling.className = "active";
+      break;
+    }
+  }
+
+}
+
+
+
+// var active = document.getElementsByClassName("active");
+
+// 
+
+// var cells = document.getElementById("player1_strip").getElementsByTagName("td");
+
+// console.log(cells);
+
+// for (i = 0; i < cells.length; i++){
+//   cells[i].addEventListener("click", updatePosition);
+//   cells[i].addEventListener("dblclick", uncheck);
+// }
+
+
+// function updatePosition(){
+//   this.className = "active";
+// }
+
+// function uncheck(){
+//   this.className = "";
+// }
+
+// document.addEventListener("keydown", itemKeyPress);
+
+// function itemKeyPress(event){
+//   if (event.which === 38){
+//     console.log("That was the up key");
+//   }
+// }
+
+// function updatePlayerPosition(activePlayer) {
+//   for (i = 0; i < cells.length; i++){
+//     if (cells[i].className === "active"){
+//       console.log("active cell is number: ", cells[i]);
+//       cells[i].className = "";
+//       cells[i].nextElementSibling.className = "active";
+//       break;
+//     }
+//   }
+// }
+
+// updatePlayerPosition(player1);
+
+//
+// document.onkeydown = function(e) {
+//     switch (e.keyCode) {
+//         case 37:
+//             alert('left');
+//             break;
+//         case 38:
+//             alert('up');
+//             break;
+//         case 39:
+//             alert('right');
+//             break;
+//         case 40:
+//             alert('down');
+//             break;
+//     }
+// };
