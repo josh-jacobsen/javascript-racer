28c82ec384c3e8a2b5ee938a7ad4e6fbf97d2999
josh-jacobsen <joshjacobsen5@gmail.com>
2016-05-04 12:28:28 +1200

    further testing

--- scripts/script.js
+++ scripts/script.js
@@ -184,7 +184,7 @@ function start_game() {
 
         boardSetup();
         var ship = document.getElementById("ship");
-        ship.style.backgroundImage="url('images/ship.jpg')";
+        ship.style.backgroundImage="url('images/ship-t.png')";
 
         var sub = document.getElementById("sub");
         sub.style.backgroundImage="url('images/submarine-t.png')";
